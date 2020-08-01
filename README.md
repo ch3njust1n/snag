52fb915bc45389de8be9491a4f64202dc6290bfe
Justin Chen <ch3njus@gmail.com>
2020-08-01 11:54:57 -0400

    Removed rename

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # snag
+Snag videos from Instagram and YouTube with Pystagram and Pytube
+
+### Setup
+`sh setup.sh`
+
+### Usage
+```
+--link -l YouTube or Instagram link. (required)
+--name -n File name. Required for Instagram videos.
+--dst  -d Save destination (default: queue)
+```
+
+### Example
+
+#### Single video
+```
+$ python snap.py -l https://www.youtube.com/watch?v=QlsaQZz6418
+```
+
+#### Youtube playlist
+```
+$ python snap.py -l https://www.youtube.com/playlist?list=PLoROMvodv4rMiGQp3WXShtMGgzqpfVfbU
+```
+
+#### Instagram video
+```
+$ python snap.py -l https://www.instagram.com/p/BuEelMGnYpW/ -n dope
+```
---
+++ snag.py
@@ -0,0 +1,36 @@
+import os
+import sys
+import argparse
+from pystagram import Instagram
+from pytube import YouTube, Playlist
+
+
+def download_video(url, dst):
+    yt = YouTube(url, dst)
+    yt.streams.get_highest_resolution().download(dst)
+    print(f'downloaded {yt.title}')
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--link', '-l', type=str, required=True, help='YouTube or Instagram link. (required)')
+    parser.add_argument('--name', '-n', type=str, help='File name. Required for Instagram videos.')
+    parser.add_argument('--dst', '-d', type=str, default='downloads', help='Save destination (default: downloads)')
+    args = parser.parse_args()
+
+    if not os.path.exists(args.dst): 
+        os.makedirs(args.dst)
+
+    if 'youtube' in args.link:
+        if 'playlist' in args.link: Playlist(args.link).download_all(args.dst)
+        else: download_video(args.link, args.dst)
+    elif 'instagram' in args.link:
+        if len(args.name) == 0:
+            raise Exception('Filename required for Instagram videos')
+            
+        gram = Instagram(args.link)
+        gram.download(dst='queue', filename=args.name)
+        print(f'downloaded {args.link if len(args.name) == 0 else args.name}')
+
+if __name__ == '__main__':
+    main()
